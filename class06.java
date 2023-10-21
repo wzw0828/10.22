fe28790385854445732e4a4325398c07e0f05cb8
wzw0828 <108780167+wzw0828@users.noreply.github.com>
2023-10-22 03:05:16 +0800

    Update class06.java

--- class06.java
+++ class06.java
@@ -12,14 +12,14 @@ class CData extends CTriangle {
     }
     public void area() {
         super.show(); 
-        double t = 0.5 * base * height; // 計算三角形面積。
-        System.out.println("area=" + t); // 顯示面積。
+        double t = 0.5 * base * height; // 計算三角形面積
+        System.out.println("area=" + t); // 顯示面積
     }
 }
 
 public class class06 {
     public static void main(String[] args) {
-        CData obj = new CData(3, 8); // 創建一個具有底 3 高 8 的 CData 實例。
-        obj.area(); // 呼叫 'area' 方法以顯示三角形的資訊和其面積。
+        CData obj = new CData(3, 8); // 創建一個具有底 3 高 8 的 CData 實例
+        obj.area(); // 呼叫 'area' 方法以顯示三角形的資訊和其面積
     }
 }
